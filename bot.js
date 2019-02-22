21bb94645e0de41c10ee4a07bdf13cf3987a7061
c-aksgar <akshi.garg@assaabloy.com>
2019-02-22 15:35:58 +0530

    handling greet and help intent. Adding state management

--- bot.js
+++ bot.js
@@ -2,26 +2,156 @@
 // Licensed under the MIT License.
 
 const { ActivityTypes } = require('botbuilder');
+const { LuisRecognizer} = require('botbuilder-ai');
 const { WelcomeUser } = require('./welcome_user');
+// State Accessor Properties
+const CONVERSATION_STATE_ACCESSOR = 'conversationData';
+const USER_STATE_ACCESSOR = 'userData';
 
+// Luis Intents
+const GREETING = "Greeting";
+const NONE = "None";
+const HOLIDAY = "Upcoming Holidays";
+const HELP = "Help";
+// Luis Result Entities
+const LEAVE_REQUESTS = "leave_requests";
+const APPLY_ACTION = "apply";
+const SHOW_ACTION = "show";
+const Action_Types = "action_types";
+
+const detail = {
+    none: 'none',
+    date: 'date',
+    reason: 'reason',
+    comment: 'comment',
+    confirm: 'confirm'
+};
 
 class NagarroLeaveManagerBot {
-    /**
-     *
-     * @param {TurnContext} on turn context object.
-     */
+
+    constructor(application, luisPredictionOptions, conversationState, userState) {
+        this.luisRecognizer = new LuisRecognizer(
+            application,
+            luisPredictionOptions,
+            true
+        );
+        this.conversationStateAccessor = conversationState.createProperty(CONVERSATION_STATE_ACCESSOR);
+        this.userStateAccessor = userState.createProperty(USER_STATE_ACCESSOR);
+        this.conversationState = conversationState;
+        this.userState = userState;
+
+        this.greet = new WelcomeUser();
+    }
+
     async onTurn(turnContext) {
         const activityType = turnContext.activity.type;
 
         switch (activityType) {
             case ActivityTypes.Message:
-                await turnContext.sendActivity(`You said '${turnContext.activity.text}'`);
+                const userProfile = await this.userStateAccessor.get(turnContext, {});
+
+                const conversationFlow = await this.conversationStateAccessor.get(turnContext, {
+                    luisResultForAskedQuestion: [],
+                    promptedForEmployeeId: false,
+                    promptedForLeaveRequestDetails: detail.none
+                });
+
+                // If this is not the first message from the user, prompt user for employee id else process the message by its intent. 
+                if (conversationFlow.promptedForEmployeeId) {
+                    // If the user wants to apply for a leave, (s)he is required to enter some related information, after that process the intent.
+                    // If the user wants to do anything other than applying for the leave, process the intent. 
+                    if (conversationFlow.promptedForLeaveRequestDetails === detail.none) {
+                        userProfile.id = userProfile.id || turnContext.activity.text;
+                        await this.userStateAccessor.set(turnContext, userProfile);
+                        await this.userState.saveChanges(turnContext);
+
+                        // fetch the information of Luis result from conversation state (if present) otherwise, call LUIS api.
+                        const { topIntent, entities } = await this.fetchLuisResult(conversationFlow, turnContext);
+
+                        await this.conversationStateAccessor.set(turnContext, conversationFlow);
+                        await this.conversationState.saveChanges(turnContext);
+
+                        switch (topIntent) {
+                            case GREETING:
+                                await turnContext.sendActivity("Hi!! How may I help you?");
+                                break;
+                            case HELP:
+                                await this.greet.giveIntroduction(turnContext);
+                                break;
+                            case HOLIDAY:
+                                const holidayCalendar = new HolidayCalendar();
+                                await turnContext.sendActivity(holidayCalendar.listHolidays(turnContext, entities));
+                                break;
+                            case LEAVE_REQUESTS:
+                                const leaveRequestManager = new LeaveRequestManager(this.userStateAccessor);
+                                if (entities[Action_Types]) {
+                                    if (entities[Action_Types][0].includes(APPLY_ACTION)) {
+                                        return await this.applyForLeave(userProfile, entities, turnContext, conversationFlow);
+                                    } else if (entities[Action_Types][0].includes(SHOW_ACTION)) {
+                                        return turnContext.sendActivity(leaveRequestManager.viewSubmittedRequests(userProfile, turnContext, entities));
+                                    }
+                                } else {
+                                    await turnContext.sendActivity("I didn't understand your query.");
+                                }
+                                break;
+                            case NONE:
+                                await turnContext.sendActivity("I didn't understand your query.");
+                                break;
+                        }
+                        // if (topIntent === GREETING || topIntent === HELP) {
+                        //     await this.welcomeUser.giveIntroduction(turnContext);
+                        // } else if (topIntent === HOLIDAY) {
+                        //     const holidayCalendar = new HolidayCalendar();
+                        //     await turnContext.sendActivity(holidayCalendar.listHolidays(turnContext, result.entities));
+                        // }
+                        // else if (topIntent === LEAVE_REQUESTS) {
+                        //     const leaveRequestManager = new LeaveRequestManager(this.userStateAccessor);
+                        //     if (result.entities[Action_Types]) {
+                        //         if (result.entities[Action_Types][0].includes(APPLY_ACTION)) {
+                        //             return await this.applyForLeave(userProfile, result.entities, turnContext, conversationFlow);
+                        //         } else if (result.entities[Action_Types][0].includes(SHOW_ACTION)) {
+                        //             return turnContext.sendActivity(leaveRequestManager.viewSubmittedRequests(userProfile, turnContext, result.entities));
+                        //         }
+                        //     } else {
+                        //         await turnContext.sendActivity("I didn't understand your query.");
+                        //     }
+                        // } else if (topIntent === NONE) {
+                        //     await turnContext.sendActivity("I didn't understand your query.");
+                        // }
+                    } else {
+
+                    }
+
+                } else {
+                    await this.promptForEmployeeId(conversationFlow, turnContext);
+                }
                 break;
+
             case ActivityTypes.ConversationUpdate:
-                this.welcomeUser = new WelcomeUser();
-                await this.welcomeUser.welcomeUser(turnContext);
+                await this.welcomeUser(turnContext);
                 break;
         }
+    }
+
+    async fetchLuisResult(conversationFlow, turnContext) {
+        const result = conversationFlow.luisResultForAskedQuestion[0] || await this.luisRecognizer.recognize(turnContext);
+        const topIntent = result.luisResult.topScoringIntent.intent;
+        const entities = result.entities;
+        conversationFlow.luisResultForAskedQuestion.length = [];
+        return { topIntent, entities };
+    }
+
+    async welcomeUser(turnContext) {
+        await this.greet.welcomeUser(turnContext);
+    }
+
+    async promptForEmployeeId(conversationFlow, turnContext) {
+        const result = await this.luisRecognizer.recognize(turnContext);
+        conversationFlow.luisResultForAskedQuestion.push(result);
+        conversationFlow.promptedForEmployeeId = true;
+        await turnContext.sendActivity('Please provide your employee id.');
+        await this.conversationStateAccessor.set(turnContext, conversationFlow);
+        await this.conversationState.saveChanges(turnContext);
     }
 }
 
--- index.js
+++ index.js
@@ -1,81 +1,108 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const dotenv = require('dotenv');
+// Import required packages
 const path = require('path');
 const restify = require('restify');
 
-// Import required bot services.
-// See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const { BotFrameworkAdapter } = require('botbuilder');
-
-// Import required bot configuration.
+// Import required bot services. See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const {
+    BotFrameworkAdapter,
+    MemoryStorage,
+    ConversationState,
+    UserState } = require('botbuilder');// Import required bot configuration.
 const { BotConfiguration } = require('botframework-config');
 
-// This bot's main dialog.
 const { NagarroLeaveManagerBot } = require('./bot');
 
 // Read botFilePath and botFileSecret from .env file
 // Note: Ensure you have a .env file and include botFilePath and botFileSecret.
 const ENV_FILE = path.join(__dirname, '.env');
-dotenv.config({ path: ENV_FILE });
+const env = require('dotenv').config({ path: ENV_FILE });
 
-// bot endpoint name as defined in .bot file
+// Get the .bot file path
 // See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
-const DEV_ENVIRONMENT = 'development';
-
-// bot name as defined in .bot file
-// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
-const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
-
-// Create HTTP server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
-    console.log(`\nTo talk to your bot, open nagarro-leave-manager.bot file in the Emulator`);
-});
-
-// .bot file path
 const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
-
-// Read bot configuration from .bot file.
 let botConfig;
 try {
+    // Read bot configuration from .bot file.
     botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
 } catch (err) {
     console.error(`\nError reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
-    console.error(`\n - The botFileSecret is available under appsettings for your Azure Bot Service bot.`);
+    console.error(`\n - You can find the botFilePath and botFileSecret in the Azure App Service application settings.`);
     console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.`);
     console.error(`\n - See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.\n\n`);
     process.exit();
 }
 
-// Get bot endpoint configuration by service name
-const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
+// For local development configuration as defined in .bot file
+const DEV_ENVIRONMENT = 'development';
 
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration.
+// Define name of the endpoint configuration section from the .bot file
+const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
+
+// Get bot endpoint configuration by service name
+// Bot configuration as defined in .bot file
+const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
+const LUIS_CONFIGURATION = process.env.luisAppName;
+const luisConfig = botConfig.findServiceByNameOrId(LUIS_CONFIGURATION);
+const luisApplication = {
+    applicationId: luisConfig.appId,
+    endpointKey: luisConfig.authoringKey,
+    azureRegion: luisConfig.region,
+}
+// Create bot adapter.
+// See https://aka.ms/about-bot-adapter to learn more about bot adapter.
 const adapter = new BotFrameworkAdapter({
     appId: endpointConfig.appId || process.env.microsoftAppID,
-    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
+    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword,
+    channelService: process.env.ChannelService,
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 
-// Catch-all for errors.
+// Catch-all for any unhandled errors in your bot.
 adapter.onTurnError = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
-    console.error(`\n [onTurnError]: ${ error }`);
+    console.error(`\n [onTurnError]: ${error}`);
     // Send a message to the user
     await context.sendActivity(`Oops. Something went wrong!`);
+    // Clear out state
+    await conversationState.clear(context);
+    // Save state changes.
+    await conversationState.saveChanges(context);
 };
 
-// Create the main dialog.
-const myBot = new NagarroLeaveManagerBot();
+// Define a state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
+// A bot requires a state store to persist the dialog and user state between messages.
+let conversationState;
 
-// Listen for incoming requests.
+// For local development, in-memory storage is used.
+// CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
+// is restarted, anything stored in memory will be gone.
+const memoryStorage = new MemoryStorage();
+conversationState = new ConversationState(memoryStorage);
+const userState = new UserState(memoryStorage);
+const luisPredictionOptions = {
+    includeAllIntents: true,
+    log: true,
+    staging: false
+};
+
+// Create HTTP server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo talk to your bot, open echoBot-with-counter.bot file in the Emulator`);
+});
+
+const leaveManagerBot =
+    new NagarroLeaveManagerBot(luisApplication, luisPredictionOptions, conversationState, userState);
+
+// Listen for incoming activities and route them to your bot main dialog.
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
         // Route to main dialog.
-        await myBot.onTurn(context);
+        await leaveManagerBot.onTurn(context);
     });
 });
